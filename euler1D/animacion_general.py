bf31bf03754719fb0083136bc6f13e20a23260f8
Rodrigo Castillo <79603625+highchen147@users.noreply.github.com>
2024-01-23 22:45:59 -0600

    .

--- euler1D/animacion_general.py
+++ euler1D/animacion_general.py
@@ -42,7 +42,7 @@ def animacion(data: pd.DataFrame, subset: pd.DataFrame, cantidad: str, margen: f
     ax.set_title("{}, t={} s".format(cantidad, t))
 
 # importar datos
-folder_data = "gaussiana41"
+folder_data = "sin_fronteras193"
 data_d = pd.read_csv("data/" + folder_data + "/densidad.dat", delimiter='\t', skip_blank_lines=False)
 data_d.columns = ["t", "x", "u"]
 data_p = pd.read_csv("data/" + folder_data + "/presion.dat", delimiter="\t", skip_blank_lines=False)
